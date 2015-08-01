f1e56c455b323abfac51473ed759ec2acebd5b60
Aung Myint Thein Aug 2012 Key <thein.aungmyint@gmail.com>
2015-08-02 01:00:54 +0800

    mysql is updated

--- getContributions.php
+++ getContributions.php
@@ -13,7 +13,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT count(*) as count FROM user_inputs;";
+$sql = "SELECT count(*) as count FROM user_inputs where latitude > 0;";
 $result = $conn->query($sql);
 if( $result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
