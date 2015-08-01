10bea6a28615f621e78f602c7380d590766c5eb9
Aung Myint Thein Aug 2012 Key <thein.aungmyint@gmail.com>
2015-08-01 11:37:11 +0800

    finished first version to collect the data. next step is to create visualization

---
+++ getContributions.php
@@ -0,0 +1,27 @@
+<?php
+date_default_timezone_set('Asia/Singapore');
+
+$servername = "localhost";
+$username = "root";
+$password = "131654AMtnewton";
+$dbname = "myanmar_data";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT count(*) as count FROM user_inputs;";
+$result = $conn->query($sql);
+if( $result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "Total number of contribution: ".$row["count"];
+    }
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?> 
---
+++ save.php
@@ -0,0 +1,26 @@
+<?php
+date_default_timezone_set('Asia/Singapore');
+
+$servername = "localhost";
+$username = "root";
+$password = "131654AMtnewton";
+$dbname = "myanmar_data";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO user_inputs (latitude, longitude, timestamp)
+VALUES (".$_POST["lat"].", ".$_POST["long"].", \"".date("Y-m-d H:i:s")."\")";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
