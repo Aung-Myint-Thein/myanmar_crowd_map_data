c1fac1fda5cb6cf6f05ff0347823e5e4e7f58267
Aung Myint Thein Aug 2012 Key <thein.aungmyint@gmail.com>
2015-08-02 23:18:15 +0800

    trying to include the Contribut text in home page

--- getContributions.php
+++ getContributions.php
@@ -12,7 +12,8 @@ $sql = "SELECT count(*) as count FROM user_inputs where latitude > 0;";
 $result = $conn->query($sql);
 if( $result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-        echo "Total number of contribution: ".$row["count"];
+        echo "No. of contribution: ".$row["count"].
+        	". <a href=\"location.html\">Contribute Data Here</a>";
     }
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
