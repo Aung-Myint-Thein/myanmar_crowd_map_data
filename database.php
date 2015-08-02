4c554dc874c659da24d06638caf35d73fa7a502b
Aung Myint Thein Aug 2012 Key <thein.aungmyint@gmail.com>
2015-08-02 23:07:55 +0800

    geo coding is added

--- database.php
+++ database.php
@@ -3,8 +3,8 @@ date_default_timezone_set('Asia/Singapore');
 
 // replace the following parameters with your server name, username, password and database name
 
-$servername = "xxxx";
-$username = "xxxx";
-$password = "xxxx";
-$dbname = "xxxx";
+$servername = "x";
+$username = "x";
+$password = "x";
+$dbname = "x";
 ?>
